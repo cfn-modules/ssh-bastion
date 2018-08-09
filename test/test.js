763578cc045ea0abe523c4f7a2599224ea55a53a
Michael Wittig <michael@widdix.de>
2018-08-09 09:51:35 +0200

    add tests

---
+++ test/test.js
@@ -0,0 +1,37 @@
+const test = require('ava');
+const cfntest = require('@cfn-modules/test');
+
+test('defaults', async t => {
+  const stackName = cfntest.stackName();
+  try {
+    t.log(await cfntest.createStack(`${__dirname}/defaults.yml`, stackName, {}));
+    // what could we test here?
+  } finally {
+    t.log(await cfntest.deleteStack(stackName));
+    t.pass();
+  }
+});
+
+test('key-name', async t => {
+  const stackName = cfntest.stackName();
+  const keyName = cfntest.keyName();
+  try {
+    const key = await cfntest.createKey(keyName);
+    try {
+      t.log(await cfntest.createStack(`${__dirname}/key-name.yml`, stackName, {
+        KeyName: keyName
+      }));
+      const outputs = await cfntest.getStackOutputs(stackName);
+      t.log(outputs);
+      await cfntest.probeSSH(`ec2-user@${outputs.PublicIpAddress}`, key);
+    } finally {
+      t.log(await cfntest.deleteStack(stackName));
+    }
+  } finally {
+    t.log(await cfntest.deleteKey(keyName));
+    t.pass();
+  }
+});
+
+// TODO test AlertingModule
+// TODO test HostedZoneModule
